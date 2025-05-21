624f703eac23371e7f51e72744e16f41d8d7e4b1
Rogério Alves <rogerio.alves651@gmail.com>
2025-05-21 13:59:43 -0300

    Atualiza mensagem de log para incluir URL do servidor.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
 
 app.get('/', (req, res) => {
